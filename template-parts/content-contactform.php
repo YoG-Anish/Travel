0442d88a84d7cb38a996731b7a8a1f6aa6809d3b
YoG-Anish <yogesh.anish01@gmail.com>
2026-05-07 15:25:28 +0545

    home page content dynamic complete

--- template-parts/content-contactform.php
+++ template-parts/content-contactform.php
@@ -1,15 +1,15 @@
 <section class="newsletter-section">
-        <div class="mountain-cloud-bg">
-            <div class="container text-center">
-                <span class="section-tag"> <?php echo get_theme_mod('contactform_tagline'); ?> </span>
-                <h2 class="section-heading">
-                    <?php echo get_theme_mod('contactform_heading', 'Contact Us'); ?> 
-                </h2>
-            </div>
+    <div class="mountain-cloud-bg">
+        <div class="container text-center">
+            <span class="section-tag"> <?php echo get_theme_mod('contactform_tagline'); ?> </span>
+            <h2 class="section-heading">
+                <?php echo get_theme_mod('contactform_heading'); ?>
+            </h2>
         </div>
-        <div class="cloud-bg">
-            <div class="container newsletter-form-wrapper">
-                <?php echo do_shortcode('[contact-form-7 id="9a17d08" title="travel contact"]'); ?>
-            </div>
+    </div>
+    <div class="cloud-bg">
+        <div class="container newsletter-form-wrapper">
+            <?php echo do_shortcode('[contact-form-7 id="9a17d08" title="travel contact"]'); ?>
         </div>
-    </section>
+    </div>
+</section>
--- templates/template-home.php
+++ templates/template-home.php
@@ -8,69 +8,53 @@ get_header();
     <section id="things-to-do" class="features-section section-padding">
         <div class="container features-grid">
             <div class="features-intro">
-                <span class="section-tag">FIND INSPIRATION FOR YOUR TRIP</span>
-                <h2 class="section-heading">
-                    Tailored Travel <br />
-                    Guided By <span class="highlight">Experts</span> <br />
-                    And Powered By <br />
-                    Technology
-                </h2>
-                <p class="section-text">
-                    If you're looking for that perfect green and safe ossis, you've
-                    come to the right place. Slovenia is the green heart of Europe,
-                    where everyone can find sormething for themselves
-                </p>
-                <p class="section-text">
-                    We are ready to offer you the best we have With great deal of
-                    responsibility, your visit to flavenia will be enjoyable and
-                    unforgettabile
-                </p>
+                <span class="section-tag"><?php echo get_field('home_section1_tagline'); ?></span>
+                <?php echo get_field('home_section1_heading'); ?>
+                <?php echo get_field('home_section1_description'); ?>
             </div>
             <div class="features-cards">
                 <div class="feature-card">
                     <div class="feature-icon icon-person">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/Service.svg" alt="service" />
+                        <?php
+                        $section1_parts = get_field('section1_parts');
+                        $part1_image = $section1_parts['part1_image'];
+                        $part1_content = $section1_parts['part1_content'];
+                        $part2_image = $section1_parts['part2_image'];
+                        $part2_content = $section1_parts['part2_content'];
+                        $part3_image = $section1_parts['part3_image'];
+                        $part3_content = $section1_parts['part3_content'];
+                        $part4_image = $section1_parts['part4_image'];
+                        $part4_content = $section1_parts['part4_content'];
+                        if ($part1_image) : ?>
+                            <img src="<?php echo esc_url($part1_image['url']); ?>" alt="<?php echo esc_attr($part1_image['alt']); ?>">
+                        <?php endif; ?>
                     </div>
-                    <h3 class="feature-card-title">Personalized Service</h3>
-                    <p class="feature-card-desc">
-                        One-to-one service designed to fit your needs and preferences,
-                        with around-the-clock support while you travel.
-                    </p>
+                    <?php echo $part1_content; ?>
                 </div>
-
                 <div class="feature-card">
                     <div class="feature-icon icon-tech">
-                        <img
-                            src="./images/Mobile Navigator.svg"
-                            alt="mobile navigator" />
+                        <?php
+                        if ($part2_image) : ?>
+                            <img src="<?php echo esc_url($part2_image['url']); ?>" alt="<?php echo esc_attr($part2_image['alt']); ?>">
+                        <?php endif; ?>
                     </div>
-                    <h3 class="feature-card-title">Technology-Driven</h3>
-                    <p class="feature-card-desc">
-                        Easy-to-use tools to keep trip details at your fingertips and
-                        simplify your travel.
-                    </p>
+                    <?php echo $part2_content; ?>
                 </div>
                 <div class="feature-card">
                     <div class="feature-icon icon-connections">
-                        <img
-                            src="./images/Connected People.svg"
-                            alt="connected people" />
+                        <?php if ($part3_image) : ?>
+                            <img src="<?php echo esc_url($part3_image['url']); ?>" alt="<?php echo esc_attr($part3_image['alt']); ?>">
+                        <?php endif; ?>
                     </div>
-                    <h3 class="feature-card-title">Insider Connections</h3>
-                    <p class="feature-card-desc">
-                        We leverage our global network to provide preferred rates,
-                        upgrades, and experiences you won't find online.
-                    </p>
+                    <?php echo $part3_content; ?>
                 </div>
                 <div class="feature-card">
                     <div class="feature-icon icon-employee">
-                        <img src="./images/Name Tag.svg" alt="name tag" />
+                        <?php if ($part4_image) : ?>
+                            <img src="<?php echo esc_url($part4_image['url']); ?>" alt="<?php echo esc_attr($part4_image['alt']); ?>">
+                        <?php endif; ?>
                     </div>
-                    <h3 class="feature-card-title">Employee-Owned</h3>
-                    <p class="feature-card-desc">
-                        We leverage our global network to provide preferred rates,
-                        upgrades, and experiences you won't find online.
-                    </p>
+                    <?php echo $part4_content; ?>
                 </div>
             </div>
         </div>
@@ -79,15 +63,8 @@ get_header();
     <section id="places" class="places-section section-padding bg-light">
         <div class="container">
             <div class="section-header">
-                <span class="section-tag">GET INSPIRED</span>
-                <h2 class="section-heading">
-                    <span class="highlight">Inspiring</span> Places
-                </h2>
-                <p class="section-desc desc-width">
-                    Wherever the path may lead you in the future, you will always find
-                    something to make your heart sing. The greatest Slovenian
-                    treasures await. How will you feel Slovenia?
-                </p>
+                <span class="section-tag"><?php the_field('home_section2_tagline'); ?></span>
+                <?php the_field('home_section2_content'); ?>
             </div>
             <div class="places-grid">
                 <?php
@@ -126,15 +103,8 @@ get_header();
     <section id="plan" class="style-section section-padding">
         <div class="container">
             <div class="section-header">
-                <span class="section-tag">TRAVEL IN STYLE</span>
-                <h2 class="section-heading">
-                    Find Travel Inspiration By <span class="highlight">Style</span>
-                </h2>
-                <p class="section-desc desc-width">
-                    Wherever the path may lead you in the future, you will always find
-                    something to make your heart sing. The greatest Slovenian
-                    treasures await. How will you feel Slovenia?
-                </p>
+                <span class="section-tag"><?php the_field('home_section4_tagline'); ?></span>
+                <?php the_field('home_section4_content'); ?>
             </div>
 
             <div class="splide style-slider">
@@ -193,10 +163,10 @@ get_header();
         <div class="container">
             <div class="section-header center-header">
                 <span class="section-tag">
-                    GET A GLIMPSE OF WHAT YOU CAN EXPERIENCE
+                    <?php the_field('home_section5_tagline'); ?>
                 </span>
                 <h2 class="section-heading">
-                    Inspiring <span class="highlight">Stories</span>
+                    <?php the_field('home_section5_content'); ?>
                 </h2>
             </div>
             <div class="stories-grid">
@@ -239,7 +209,7 @@ get_header();
     <?php get_template_part('template-parts/content', 'testonomial'); ?>
     <?php get_template_part('template-parts/content', 'contactform'); ?>
     <?php get_template_part('template-parts/content', 'contactsection'); ?>
-    
+
 </main>
 
 <?php get_footer(); ?>
